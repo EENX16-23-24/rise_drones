b14bdf514d5510162e908e3555a09297011e22f4
Albin Falsen Lindqvist <albin.falsen@gmail.com>
2023-02-21 14:45:55 +0100

    Added code that reads csv into mission

--- src/dev/app_fence_mission.py
+++ src/dev/app_fence_mission.py
@@ -22,8 +22,8 @@ import dss.client
 
 #--------------------------------------------------------------------#
 
-__author__ = ''
-__version__ = '0.1.0'
+__author__ = 'Lennart Ochel <lennart.ochel@ri.se>, Andreas Gising <andreas.gising@ri.se>, Kristoffer Bergman <kristoffer.bergman@ri.se>, Hanna Müller <hanna.muller@ri.se>, Joel Nordahl'
+__version__ = '0.2.0'
 __copyright__ = 'Copyright (c) 2022, RISE'
 __status__ = 'development'
 
